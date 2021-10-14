a5a2483d918c7eee7cf31a7bc87bc6f3017645df
Jonathan Remmers <jona.remm@gmail.com>
2021-10-14 23:22:44 +0200

    improve margins, accessibility, update readme

--- README.md
+++ README.md
@@ -30,7 +30,7 @@ Users should be able to:
 
 ### Links
 
-- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Solution URL: [Solution](https://www.frontendmentor.io/solutions/mobilefirst-with-vuejs-and-tailwindcss-jtqJ7j3sM)
 - Live Site URL: [Live Site](https://order-summary-component-main-omega.vercel.app/)
 
 ## My process
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,10 +2,10 @@
 </script>
 
 <template>
-    <div class="mx-6 mt-16 rounded-2xl overflow-hidden bg-white shadow-2xl xs:max-w-md xs:mx-auto">
+    <main class="mx-6 mt-20 rounded-2xl overflow-hidden bg-white shadow-2xl xs:max-w-md xs:mx-auto xs:mt-28">
         <img src="../assets/illustration-hero.svg" alt="Woman with headphones on and a cellphone in hand, dancing in front of a night time skyline amidst floating musical notes">
-        <div class="p-6 pb-4 xs:p-12 xs:pb-8">
-            <h2 class="mt-2 font-black text-2xl text-center text-gray-800">Order Summary</h2>
+        <div class="p-6 xs:p-12">
+            <h1 class="mt-2 font-black text-2xl text-center text-gray-800 xs:text-3xl">Order Summary</h1>
             <p class="mt-4 px-1 text-center">
                 You can now listen to millions of songs, audiobooks, and podcasts on any
                 device anywhere you like!
@@ -20,9 +20,9 @@
                 <a href="#" class="text-xs font-bold text-indigo-700 underline xs:text-sm hover:text-indigo-500 hover:no-underline">Change</a>
             </div>
             <button class="w-full mt-6 py-3 shadow-2xl rounded-xl bg-indigo-700 font-bold text-gray-200 hover:bg-indigo-500">Proceed to Payment</button>
-            <button class="w-full mt-2 py-3 rounded-xl font-bold hover:text-gray-800">Cancel Order</button>
+            <button class="w-full mt-4 py-3 rounded-xl font-bold hover:text-gray-800">Cancel Order</button>
         </div>
-    </div>
+    </main>
     <div class="attribution absolute bottom-0 left-0 right-0 mx-auto">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
         Coded by <a href="https://github.com/JonathanRe">jona.re</a>.
