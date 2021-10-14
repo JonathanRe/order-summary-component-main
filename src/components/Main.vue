1f34ef7a5c9c3fb3175cb14f21c1e6d73c1c383f
Jonathan Remmers <jona.remm@gmail.com>
2021-10-14 19:51:36 +0200

    mobile layout for price plan component

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-    <div class="mx-6 mt-16 rounded-2xl overflow-hidden bg-very-pale-blue shadow-2xl">
+    <div class="mx-6 mt-16 rounded-2xl overflow-hidden bg-white shadow-2xl">
         <img src="../assets/illustration-hero.svg" alt="Woman with headphones on and a cellphone in hand, dancing in front of a night time skyline amidst floating musical notes">
         <div class="p-6 pb-4">
             <h2 class="mt-2 font-black text-2xl text-center text-gray-800">Order Summary</h2>
@@ -10,14 +10,17 @@
                 You can now listen to millions of songs, audiobooks, and podcasts on any
                 device anywhere you like!
             </p>
-            <div class="mt-8">
+            <div class="flex flex-row items-center mt-6 p-4 rounded-xl bg-very-pale-blue">
                 <img src="../assets/icon-music.svg" alt="Icon of a musical note">
-                <span class="font-black text-gray-800">Annual Plan</span>
-                <span>$59.99/year</span>
-                <a href="#">Change</a>
+                <div class="flex-grow ml-4 text-sm">
+                    <span class="font-black text-gray-800">Annual Plan</span>
+                    <br>
+                    <span>$59.99/year</span>
+                </div>
+                <a href="#" class="text-xs font-bold text-indigo-700 underline">Change</a>
             </div>
-            <button class="w-full mt-8 py-4 shadow-2xl rounded-xl bg-indigo-700 font-bold text-gray-200">Proceed to Payment</button>
-            <button class="w-full mt-2 py-4 rounded-xl font-bold">Cancel Order</button>
+            <button class="w-full mt-6 py-3 shadow-2xl rounded-xl bg-indigo-700 font-bold text-gray-200">Proceed to Payment</button>
+            <button class="w-full mt-2 py-3 rounded-xl font-bold">Cancel Order</button>
         </div>
     </div>
     <div class="attribution absolute bottom-0 left-0 right-0 mx-auto">
