2ccad30a654a9cc9ab0d002f4e81099b012a10d7
Jonathan Remmers <jona.remm@gmail.com>
2021-10-14 20:07:22 +0200

    add custom xs breakpoint, set max width on wrapper

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      'xs': '495px',
+      ...defaultTheme.screens,
+    },
     extend: {
       fontFamily: {
         'custom': ['"Red Hat Display"', 'sans-serif'],
